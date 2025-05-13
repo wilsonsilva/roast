bdf2da422034ec5fa88bbefe0afc07e295353ad7
Jon Allured <jon.allured@shopify.com>
2025-05-13 10:57:13 -0500

    Add coverage for initializers that raise errors

--- test/roast/initializers_test.rb
+++ test/roast/initializers_test.rb
@@ -33,18 +33,38 @@ module Roast
       end
     end
 
-    def test_with_an_initializer_file
-      test_initializer_path = File.join(Dir.pwd, "test", "fixtures", "initializers")
+    def test_with_initializer_file_that_raises
+      raises_initializer_path = File.join(Dir.pwd, "test", "fixtures", "initializers", "raises")
 
-      Roast::Initializers.stub(:initializers_path, test_initializer_path) do
+      Roast::Initializers.stub(:initializers_path, raises_initializer_path) do
+        out, err = capture_io do
+          Roast::Initializers.load_all
+        end
+
+        expected_output = <<~OUTPUT
+          ERROR: Error loading initializers: exception class/object expected
+        OUTPUT
+        assert_includes(out, expected_output)
+        expected_stderr = <<~OUTPUT
+          Loading project initializers from #{raises_initializer_path}
+          Loading initializer: #{File.join(raises_initializer_path, "hell.rb")}
+        OUTPUT
+        assert_equal(expected_stderr, err)
+      end
+    end
+
+    def test_with_an_initializer_file
+      single_initializer_path = File.join(Dir.pwd, "test", "fixtures", "initializers", "single")
+
+      Roast::Initializers.stub(:initializers_path, single_initializer_path) do
         out, err = capture_io do
           Roast::Initializers.load_all
         end
 
         assert_equal("", out)
         expected_output = <<~OUTPUT
-          Loading project initializers from #{test_initializer_path}
-          Loading initializer: #{File.join(test_initializer_path, "noop.rb")}
+          Loading project initializers from #{single_initializer_path}
+          Loading initializer: #{File.join(single_initializer_path, "noop.rb")}
         OUTPUT
         assert_equal(expected_output, err)
       end
