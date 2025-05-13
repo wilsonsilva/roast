9bc5a8cdb8179eb789d3dcb783109b39550b3387
Jon Allured <jon.allured@shopify.com>
2025-05-13 11:01:15 -0500

    Extract helper methods for initializer paths

--- test/roast/initializers_test.rb
+++ test/roast/initializers_test.rb
@@ -4,10 +4,14 @@ require "test_helper"
 
 module Roast
   class InitializersTest < ActiveSupport::TestCase
-    def test_with_invalid_initializers_folder
-      invalid_initializer_path = File.join(Dir.pwd, "test", "fixtures", "invalid")
+    def path_for_initializers(name)
+      File.join(Dir.pwd, "test", "fixtures", "initializers", name)
+    end
 
-      Roast::Initializers.stub(:initializers_path, invalid_initializer_path) do
+    def test_with_invalid_initializers_folder
+      initializer_path = path_for_initializers("invalid")
+
+      Roast::Initializers.stub(:initializers_path, initializer_path) do
         out, err = capture_io do
           Roast::Initializers.load_all
         end
@@ -18,25 +22,25 @@ module Roast
     end
 
     def test_with_no_initializer_files
-      empty_initializer_path = File.join(Dir.pwd, "test", "fixtures", "initializers", "empty")
+      initializer_path = path_for_initializers("empty")
 
-      Roast::Initializers.stub(:initializers_path, empty_initializer_path) do
+      Roast::Initializers.stub(:initializers_path, initializer_path) do
         out, err = capture_io do
           Roast::Initializers.load_all
         end
 
         assert_equal("", out)
         expected_output = <<~OUTPUT
-          Loading project initializers from #{empty_initializer_path}
+          Loading project initializers from #{initializer_path}
         OUTPUT
         assert_equal(expected_output, err)
       end
     end
 
     def test_with_initializer_file_that_raises
-      raises_initializer_path = File.join(Dir.pwd, "test", "fixtures", "initializers", "raises")
+      initializer_path = path_for_initializers("raises")
 
-      Roast::Initializers.stub(:initializers_path, raises_initializer_path) do
+      Roast::Initializers.stub(:initializers_path, initializer_path) do
         out, err = capture_io do
           Roast::Initializers.load_all
         end
@@ -46,25 +50,25 @@ module Roast
         OUTPUT
         assert_includes(out, expected_output)
         expected_stderr = <<~OUTPUT
-          Loading project initializers from #{raises_initializer_path}
-          Loading initializer: #{File.join(raises_initializer_path, "hell.rb")}
+          Loading project initializers from #{initializer_path}
+          Loading initializer: #{File.join(initializer_path, "hell.rb")}
         OUTPUT
         assert_equal(expected_stderr, err)
       end
     end
 
     def test_with_an_initializer_file
-      single_initializer_path = File.join(Dir.pwd, "test", "fixtures", "initializers", "single")
+      initializer_path = path_for_initializers("single")
 
-      Roast::Initializers.stub(:initializers_path, single_initializer_path) do
+      Roast::Initializers.stub(:initializers_path, initializer_path) do
         out, err = capture_io do
           Roast::Initializers.load_all
         end
 
         assert_equal("", out)
         expected_output = <<~OUTPUT
-          Loading project initializers from #{single_initializer_path}
-          Loading initializer: #{File.join(single_initializer_path, "noop.rb")}
+          Loading project initializers from #{initializer_path}
+          Loading initializer: #{File.join(initializer_path, "noop.rb")}
         OUTPUT
         assert_equal(expected_output, err)
       end
